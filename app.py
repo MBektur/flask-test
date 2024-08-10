24540a95ff17fb98afe28cb8fb97a7fa67cb2cc7
MBektur <114426259+MBektur@users.noreply.github.com>
2024-08-10 17:47:27 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template 
+import random 
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return "mmm"
+
+@app.route("/mbsa")
+def mbsa():
+    return render_template('index.html')
