d25aed414e453760214c373f4655802a860d2a07
MBektur <114426259+MBektur@users.noreply.github.com>
2024-08-10 14:59:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    s = ""
     for x in range(10):
-        r = random.randint(1, 100)
-        s += "hi" + str(r) + "\n"
-    return s
+        x += x
+    return str(x)
 
 @app.route("/mbsa")
 def mbsa():
